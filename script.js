5b4e2ec46ee26b56fc29504d523728041d7854cf
ovianaulfasafitri30-rgb <oviana.ulfa.safitri30@gmail.com>
2025-12-15 12:36:13 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function scrollToMateri() {
+  document.getElementById("materi").scrollIntoView({ behavior: "smooth" });
+}
+
+const buttons = document.querySelectorAll(".accordion-btn");
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const content = btn.nextElementSibling;
+    content.style.display =
+      content.style.display === "block" ? "none" : "block";
+  });
+});
